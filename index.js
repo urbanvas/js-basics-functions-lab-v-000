d79e701b43210068b23287740681b3824662958a
Neil Seejoor <nvseejoor@gmail.com>
2018-09-28 20:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function distanceFromHqInBlocks (block) {
 function distanceFromHqInFeet (place) {
   return distanceFromHqInBlocks(place) * 264
 }
+
+function distanceTravelledInFeet(blockOne, blockTwo) {
+  return Math.abs(blockOne - blockTwo) * 264
+}
