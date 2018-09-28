630c99cf3a9e6f5e5eb36cf31309f3b0e601d1a2
Neil Seejoor <nvseejoor@gmail.com>
2018-09-28 20:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function distanceFromHqInFeet (place) {
 function distanceTravelledInFeet(blockOne, blockTwo) {
   return Math.abs(blockOne - blockTwo) * 264
 }
+
+function calculatesFarePrice(start, destination) {
+  const feet = distanceTravelledInFeet(start, destination)
+  if (feet < 400) {
+    return 0
+  } else if (feet > 400 && feet < 2000) {
+    return feet * .02
+  } else if (feet > 2000 && feet < 2500) {
+    return 25
+  } else if (feet > 2500) {
+    return 'cannot travel that far'
+  }
+}
